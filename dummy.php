ff6aae0347f0060bc521c41dbc22f170478fca0b
AR Setiawan <arrasy.setiawan@gmail.com>
2016-09-09 04:56:38 +0700

    Commit Pertama

---
+++ dummy.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/lightbox.min.css">
+
+	</head>
+
+	<body>
+		<a href="img/slide-6.jpeg" data-title="captioneee" data-lightbox="sesat">
+			<img src="img/slide-6.jpeg" width="200" class="img-thumbnail" alt="">
+		</a>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/lightbox.min.js"></script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,276 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/lightbox.min.css">
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		<title>AR's Web</title>
+	</head>
+	<body>
+		<!-- Header start -->
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-default navbar-fixed-top" id="nav">
+					<div class="container" id="menu-layout">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" id="logo" href="#">ARS</a>
+						</div>
+						<div id="navbar" class="navbar-collapse collapse">
+							<ul class="nav navbar-nav navbar-right" id="menu-item">
+								<li><a href="#">Home</a></li>
+								<li><a href="#about-layout">About</a></li>
+								<li><a href="#gallery-layout">Gallery</a></li>
+								<li><a href="#contact-layout">Contact</a></li>
+							</ul>
+						</div><!--/.nav-collapse -->
+					</div>
+				</nav>
+			</div>
+		</header>
+		<!-- Header end -->
+		<div id="page">
+			<!-- Main start -->
+			<main>
+				<div id="home-layout">
+					<div id="home-content">
+						<div class="landing-img">
+							<div class="caption">
+								<h1><b>Consciousness</b> <u>Is</u> <br><em> The Enlightenment Step</em></h1>
+							</div>
+							<img src="img/vf8EbJ.jpg" alt="" class="img-responsive"/>
+						</div>
+					</div>
+				</div>
+				<div id="about-layout">
+					<div class="row">
+						<div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2">
+							<h2>About Me</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis iste a labore voluptas earum vel tempore consequatur laborum</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="item-about col-xs-6 col-sm-4 col-md-3">
+							<div class="about-point">
+								<i class="fa fa-code"></i>
+								<h3>Coding</h3>
+								<p>onsectetur adipisicaing elit, sedo eiusmod tempor incidi et dolorerserss eerhfre mag.</p>
+							</div>
+						</div>
+						<div class="item-about col-xs-6 col-sm-4 col-md-3">
+							<div class="about-point">
+								<i class="fa fa-camera-retro"></i>
+								<h3>Photography</h3>
+								<p>onsectetur adipisicaing elit, sedo eiusmod tempor incidi et dolorerserss eerhfre mag.</p>
+							</div>
+						</div>
+						<div class="item-about col-xs-6 col-sm-4 col-md-3">
+							<div class="about-point">
+								<i class="fa fa-eye"></i>
+								<h3>Consciousness</h3>
+								<p>onsectetur adipisicaing elit, sedo eiusmod tempor incidi et dolorerserss eerhfre mag.</p>
+							</div>
+						</div>
+						<div class="item-about col-xs-6 col-sm-4 col-md-3">
+							<div class="about-point">
+								<i class="fa fa-mobile-phone"></i>
+								<h3>Mobile 1st</h3>
+								<p>onsectetur adipisicaing elit, sedo eiusmod tempor incidi et dolorerserss eerhfre mag.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div id="gallery-layout">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2">
+							<h2>Gallery</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis iste a labore voluptas earum vel tempore consequatur laborum</p>
+						</div>
+					</div>
+					<div class="row"">
+						<div class="col-md-4 gallery-item col-sm-6">
+							<figure class="gallery-point">
+								<img class="img-responsive" src="img/slide-1.png" alt="">
+								<figcaption id="gallery-details">
+									<h4>Title</h4>
+									<p>onsectetur adipisicing elit, sedo eiusmod tempor incidi et dolorerserss eerhfre mag.</p>
+									<a href="img/slide-1.png" data-lightbox="portfolio" title="">
+										<i class="fa fa-search"></i>
+									</a>
+								</figcaption>
+							</figure>
+						</div>
+						<div class="col-md-4 gallery-item col-sm-6">
+							<figure class="gallery-point">
+								<img class="img-responsive" src="img/slide-2.jpg" alt="">
+								<figcaption id="gallery-details">
+									<h4>Title</h4>
+									<p>onsectetur adipisicing elit, sedo eiusmod tempor incidi et dolorerserss eerhfre mag.</p>
+									<a href="img/slide-2.jpg" data-lightbox="portfolio" title="">
+										<i class="fa fa-search"></i>
+									</a>
+								</figcaption>
+							</figure>
+						</div>
+						<div class="col-md-4 gallery-item col-sm-6">
+							<figure class="gallery-point">
+								<img class="img-responsive" src="img/slide-3.jpg" alt="">
+								<figcaption id="gallery-details">
+									<h4>Title</h4>
+									<p>onsectetur adipisicing elit, sedo eiusmod tempor incidi et dolorerserss eerhfre mag.</p>
+									<a href="img/slide-3.jpg" data-lightbox="portfolio" title="">
+										<i class="fa fa-search"></i>
+									</a>
+								</figcaption>
+							</figure>
+						</div>
+						<div class="col-md-4 gallery-item col-sm-6">
+							<figure class="gallery-point">
+								<img class="img-responsive" src="img/slide-4.jpg" alt="">
+								<figcaption id="gallery-details">
+									<h4>Title</h4>
+									<p>onsectetur adipisicing elit, sedo eiusmod tempor incidi et dolorerserss eerhfre mag.</p>
+									<a href="img/slide-4.jpg" data-lightbox="portfolio" title="">
+										<i class="fa fa-search"></i>
+									</a>
+								</figcaption>
+							</figure>
+						</div>
+						<div class="col-md-4 gallery-item col-sm-6">
+							<figure class="gallery-point">
+								<img class="img-responsive" src="img/slide-5.jpg" alt="">
+								<figcaption id="gallery-details">
+									<h4>Title</h4>
+									<p>onsectetur adipisicing elit, sedo eiusmod tempor incidi et dolorerserss eerhfre mag.</p>
+									<a href="img/slide-5.jpg" data-lightbox="portfolio" title="">
+										<i class="fa fa-search"></i>
+									</a>
+								</figcaption>
+							</figure>
+						</div>
+						<div class="col-md-4 gallery-item col-sm-6">
+							<figure class="gallery-point">
+								<img sclass="img-responsive" src="img/slide-6.jpeg" alt="">
+								<figcaption id="gallery-details">
+									<h4>Title</h4>
+									<p>onsectetur adipisicing elit, sedo eiusmod tempor incidi et dolorerserss eerhfre mag.</p>
+									<a href="img/slide-6.jpeg" data-lightbox="portfolio" title="">
+										<i class="fa fa-search"></i>
+									</a>
+								</figcaption>
+							</figure>
+						</div>
+					</div>
+				</div>
+
+				<div id="contact-layout">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2">
+							<h2>Contact</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis iste a labore voluptas earum vel tempore consequatur laborum</p>
+						</div>
+					</div>
+					<div class="row">
+						<!-- TODO : Masih perlu direvisi -->
+						<div id="contact-content">
+							<div class="col-md-4 side-contact">
+								<form>
+									<legend><span class="fa fa-instagram"></span> Our office</legend>
+									<address>
+										<strong>ARRA-SY</strong><br>
+										Java<br>
+										Indonesia, Bumi Allah<br>
+										<abbr title="Phone">
+										P:</abbr>
+										(62) 8578 5036 166
+									</address>
+									<address>
+										<strong>AR Setiawan</strong><br>
+										<a href="mailto:#">arrasy.setiawan@gmail.com</a>
+									</address>
+									</form>
+							</div>
+							<div class="col-md-8 email-field">
+								<div class="well well-sm">
+									<form>
+										<div class="row">
+											<div class="col-md-6">
+												<div class="form-group">
+													<label for="name">
+													Name</label>
+													<input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
+												</div>
+												<div class="form-group">
+													<label for="email">
+													Email Address</label>
+													<div class="input-group">
+														<span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+													</span>
+													<input type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
+												</div>
+												<div class="form-group">
+													<label for="subject">
+													Subject</label>
+													<select id="subject" name="subject" class="form-control" required="required">
+														<option value="na" selected="">Choose One:</option>
+														<option value="service">General Customer Service</option>
+														<option value="suggestions">Suggestions</option>
+														<option value="product">Product Support</option>
+													</select>
+												</div>
+											</div>
+											<div class="col-md-6">
+												<div class="form-group">
+													<label for="name">
+													Message</label>
+													<textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+													placeholder="Message"></textarea>
+												</div>
+											</div>
+											<div class="col-md-12">
+												<button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
+												Send Message</button>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- TODO : Layout Contact berisi input nama, email, komentar -->
+					<!-- TODO : Responsive input layout -->
+				</div>
+			</main>
+			<!-- Main end -->
+		</div>
+		<!-- Footer start -->
+		<footer>
+			<!-- TODO : Desain footer dengan konsep sticky footer -->
+			<!-- <div class="container">
+				<div class="navbar navbar-default navbar-fixed-bottom">
+					<div class="text-center">
+						<p>
+							<br>
+							&copy 1437H - AR Setiawan
+						</p>
+					</div>
+				</div>
+			</div> -->
+		</footer>
+		<!-- Footer end -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/lightbox.min.js"></script>
+		<script src="js/smoothscroll.js"></script>
+		<script src="js/script.js"></script>
+	</body>
+</html>
